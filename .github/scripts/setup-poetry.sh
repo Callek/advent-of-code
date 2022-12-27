4336b3e980f61e45cc9a0fda5329b1a1463c76e2
Justin Wood <jwood@starry.com>
2022-12-26 23:14:55 -0500

    Setup Github Actions

---
+++ .github/scripts/setup-poetry.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+# Environment setup script for the local project. Intended to be used with automation
+# to create a repeatable local environment for tests to be run in. The python env
+# this script creates can be accessed at the location defined by the CI_VENV variable
+# below.
+
+set -ex;
+
+CI_CACHE=$HOME/.cache;
+POETRY_VERSION=1.2.2;
+
+mkdir --parents "$CI_CACHE";
+
+command -v python;
+python --version;
+
+curl --location https://install.python-poetry.org \
+  --output "$CI_CACHE/install-poetry.py" \
+  --silent \
+  --show-error;
+python "$CI_CACHE/install-poetry.py" \
+  --version "$POETRY_VERSION" \
+  --yes;
+poetry --version --no-ansi;
+poetry run pip --version;
+
+poetry install \
+  --sync \
+  --no-ansi;
+
+poetry env info;
+poetry run tox --version;
