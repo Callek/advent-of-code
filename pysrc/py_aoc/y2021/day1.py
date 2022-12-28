22bd19669df10c539fe07525286a3fefd3775b51
Justin Wood <jwood@starry.com>
2022-12-28 13:47:48 -0500

    2021 - Day 1 - import from https://github.com/Callek/advent-of-code-2021

---
+++ pysrc/py_aoc/y2021/day1.py
@@ -0,0 +1,52 @@
+"""2021 - Day 1"""
+import os
+
+from typing import List
+
+inputfile = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "../../../2022/days/1/input.txt")
+)
+
+
+def get_data_depths(data: str) -> List[int]:
+    """Get data for depths"""
+    depths = [int(line) for line in data.splitlines()]
+    return depths
+
+
+def part1(depths: List[int]) -> int:
+    """Part1"""
+    count = 0
+    previous = None
+    for current in depths:
+        if not previous:
+            previous = current
+            continue
+        if current > previous:
+            count += 1
+        previous = current
+    return count
+
+
+def part2(depths: List[int]) -> int:
+    """Part2"""
+    min_slice = 0
+    max_slice = 3
+    count = 0
+    while max_slice <= len(depths):
+        previous_depths = depths[min_slice:max_slice]
+        current_depths = depths[min_slice + 1 : max_slice + 1]
+        if sum(current_depths) > sum(previous_depths):
+            count += 1
+        min_slice += 1
+        max_slice += 1
+    return count
+
+
+def main() -> None:
+    """Main Logic"""
+    with open(inputfile) as infile:
+        depths = get_data_depths(infile.read())
+
+    print("Part1: ", part1(depths))
+    print("Part2: ", part2(depths))
---
+++ tests/y2021/test_day1.py
@@ -0,0 +1,34 @@
+"""Tests for day 1"""
+from py_aoc.y2021 import day1
+
+
+EXAMPLE_DATA = """199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+"""
+
+
+def test_nums() -> None:
+    """Test data extraction"""
+    assert all(isinstance(x, int) for x in day1.get_data_depths(EXAMPLE_DATA))
+
+
+def test_num_increasing() -> None:
+    """Test part 1"""
+    depths = day1.get_data_depths(EXAMPLE_DATA)
+
+    assert day1.part1(depths) == 7
+
+
+def test_sliding_window() -> None:
+    """Test part 2"""
+    depths = day1.get_data_depths(EXAMPLE_DATA)
+
+    assert day1.part2(depths) == 5
