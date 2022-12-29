e28181a39702fb0dd5986eb2a356dc6b33120947
Justin Wood <jwood@starry.com>
2022-12-29 13:39:05 -0500

    2015 - Day 2 - Part 2

--- tests/y2015/test_day2.py
+++ tests/y2015/test_day2.py
@@ -21,3 +21,22 @@ def test_part1() -> None:
     raw_data = "2x3x4\n1x1x10\n"
     boxes = day2.parse_box_dimensions(raw_data)
     assert 101 == day2.part1(boxes)
+
+
+@pytest.mark.parametrize(
+    "box,expect",
+    (
+        (day2.BOX(2, 3, 4), 34),
+        (day2.BOX(1, 1, 10), 14),
+    ),
+)
+def test_ribbon_for_present(box: day2.BOX, expect: int) -> None:
+    """Test single boxes"""
+    assert expect == day2.ribbon_for_present(box)
+
+
+def test_part2() -> None:
+    """Test Part 2"""
+    raw_data = "2x3x4\n1x1x10\n"
+    boxes = day2.parse_box_dimensions(raw_data)
+    assert 48 == day2.part2(boxes)
