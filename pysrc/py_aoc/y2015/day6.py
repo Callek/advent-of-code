4e80b541272d376c7485f5fb703f6504fd6379f9
Justin Wood <jwood@starry.com>
2022-12-29 22:11:48 -0500

    2015 - Day 6 - Part 2

--- pysrc/py_aoc/y2015/day6.py
+++ pysrc/py_aoc/y2015/day6.py
@@ -24,7 +24,7 @@ class COORD:
 class COMMAND:
     """Command"""
 
-    function: Callable[[bool], bool]
+    function: Callable[[int, bool], int]
     start: COORD
     end: COORD
 
@@ -54,32 +54,49 @@ def parse_commands(data: list[str]) -> list[COMMAND]:
     return commands
 
 
-def toggle(light: bool) -> bool:
+def toggle(light: int, second_part: bool = False) -> int:
     """Perform a toggle"""
-    return not light
+    if second_part:
+        return light + 2
+    return int(not light > 0)
 
 
-def turn_on(light: bool) -> bool:
+def turn_on(light: int, second_part: bool = False) -> int:
     """Turn on this light"""
-    assert light is not None
-    return True
+    if second_part:
+        return light + 1
+    return 1
 
 
-def turn_off(light: bool) -> bool:
+def turn_off(light: int, second_part: bool = False) -> int:
     """Turn off this light"""
-    assert light is not None
-    return False
+    if second_part:
+        if light > 0:
+            return light - 1
+        # intentional fall through to a floor of zero
+    return 0
 
 
 def part1(commands: list[COMMAND]) -> int:
     """Part 1"""
     # pylint: disable=invalid-name
-    grid: dict[tuple[int, int], bool] = {}
+    grid: dict[tuple[int, int], int] = {}
     for command in commands:
         for x in range(command.start.x, command.end.x + 1):
             for y in range(command.start.y, command.end.y + 1):
-                grid[(x, y)] = command.function(grid.get((x, y), False))
-    return len([light for light in grid.values() if light])
+                grid[(x, y)] = command.function(grid.get((x, y), 0), False)
+    return len([light for light in grid.values() if light > 0])
+
+
+def part2(commands: list[COMMAND]) -> int:
+    """Part 2"""
+    # pylint: disable=invalid-name
+    grid: dict[tuple[int, int], int] = {}
+    for command in commands:
+        for x in range(command.start.x, command.end.x + 1):
+            for y in range(command.start.y, command.end.y + 1):
+                grid[(x, y)] = command.function(grid.get((x, y), 0), True)
+    return sum(light for light in grid.values())
 
 
 def main() -> None:
@@ -89,4 +106,4 @@ def main() -> None:
     data = parse_list_of_string(raw_data)
     commands = parse_commands(data)
     print(f"{__doc__} - Part 1: {part1(commands)}")
-    # print(f"{__doc__} - Part 2: {part2(strings)}")
+    print(f"{__doc__} - Part 2: {part2(commands)}")
