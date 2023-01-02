2d56b8f9e67849d7c534fb5c5289f9c0f47f4d26
Justin Wood <jwood@starry.com>
2023-01-01 22:40:24 -0500

    2015 - Day 10 - Part 2

--- tests/y2015/test_day10.py
+++ tests/y2015/test_day10.py
@@ -9,3 +9,9 @@ def test_part1() -> None:
 
     assert "312211" == day10.do_look_and_say(TEST_DATA, iterations=5)
     assert 82350 == day10.part1(TEST_DATA)
+
+
+def test_part2() -> None:
+    """Test Part 1"""
+
+    assert 1166642 == day10.part2(TEST_DATA)
