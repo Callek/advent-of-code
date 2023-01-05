9e0ee5066eddfecf9d35f16380de1b45235f5ea5
Justin Wood <jwood@starry.com>
2023-01-04 22:30:45 -0500

    2015 - Day 12 - Part 2

--- pysrc/py_aoc/y2015/day12.py
+++ pysrc/py_aoc/y2015/day12.py
@@ -1,4 +1,4 @@
-"""2015 - Day 11"""
+"""2015 - Day 12"""
 import os
 import json
 from pathlib import Path
@@ -17,16 +17,18 @@ JSON_OBJECT_NESTING_TYPES: TypeAlias = (
 )
 
 
-def count_numbers(json_obj: JSON_OBJECT_NESTING_TYPES) -> int:
+def count_numbers(json_obj: JSON_OBJECT_NESTING_TYPES, ignore_red=False) -> int:
     """Recursively count numbers in json_obj"""
     if isinstance(json_obj, int):
         return json_obj
     if isinstance(json_obj, str):
         return 0
     if isinstance(json_obj, list):
-        return sum(count_numbers(val) for val in json_obj)
+        return sum(count_numbers(val, ignore_red=ignore_red) for val in json_obj)
     assert isinstance(json_obj, dict)
-    return sum(count_numbers(val) for val in json_obj.values())
+    if ignore_red and "red" in json_obj.values():
+        return 0
+    return sum(count_numbers(val, ignore_red=ignore_red) for val in json_obj.values())
 
 
 def part1(blob: str) -> int:
@@ -35,9 +37,15 @@ def part1(blob: str) -> int:
     return count_numbers(json_object)
 
 
+def part2(blob: str) -> int:
+    """Part 2"""
+    json_object: JSON_OBJECT_NESTING_TYPES = json.loads(blob)
+    return count_numbers(json_object, ignore_red=True)
+
+
 def main() -> None:
     """Main Logic"""
 
     raw_data = Path(inputfile).read_text().strip()
     print(f"{__doc__} - Part 1: {part1(raw_data)}")
-    # print(f"{__doc__} - Part 2: {part2(part1_pw)}")
+    print(f"{__doc__} - Part 2: {part2(raw_data)}")
--- tests/y2015/test_day12.py
+++ tests/y2015/test_day12.py
@@ -1,4 +1,4 @@
-"""2015 - Day 11"""
+"""2015 - Day 12"""
 import pytest
 from py_aoc.y2015 import day12
 
@@ -20,3 +20,18 @@ def test_part1(json_str: str, count: int) -> None:
     """Test Part 1"""
 
     assert count == day12.part1(json_str)
+
+
+@pytest.mark.parametrize(
+    ("json_str", "count"),
+    (
+        ("[1,2,3]", 6),
+        ('[1,{"c":"red","b":2},3]', 4),
+        ('{"d":"red","e":[1,2,3,4],"f":5}', 0),
+        ('[1,"red",5]', 6),
+    ),
+)
+def test_part2(json_str: str, count: int) -> None:
+    """Test Part 2"""
+
+    assert count == day12.part2(json_str)
