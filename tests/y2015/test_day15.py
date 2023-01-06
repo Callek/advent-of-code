e66b6304a08994798021e1845e657508e71057af
Justin Wood <jwood@starry.com>
2023-01-06 16:40:51 -0500

    2015 - Day 15 - Part 1

---
+++ tests/y2015/test_day15.py
@@ -0,0 +1,14 @@
+"""2015 - Day 15"""
+from py_aoc.y2015 import day15
+
+
+TEST_DATA = """Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3
+"""
+
+
+def test_part1() -> None:
+    """Test Part 1"""
+
+    ingredients = day15.parse_ingredients(TEST_DATA)
+    assert 62842880 == day15.part1(ingredients)
