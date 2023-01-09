e7a123804771ab761e72255c2564be01d06c53bb
Justin Wood <jwood@starry.com>
2023-01-08 23:16:33 -0500

    2015 - Day 16 - Part 2

--- tests/y2015/test_day16.py
+++ tests/y2015/test_day16.py
@@ -8,6 +8,9 @@ Sue 212: trees: 7, perfumes: 2, akitas: 5
 Sue 213: children: 3, goldfish: 5, vizslas: 0
 Sue 214: akitas: 6, goldfish: 0, children: 0
 Sue 215: trees: 8, akitas: 3, goldfish: 1
+Sue 322: pomeranians: 5, vizslas: 7, cats: 1
+Sue 323: perfumes: 1, trees: 6, goldfish: 0
+Sue 324: goldfish: 6, trees: 10, cars: 10
 """
 
 
@@ -16,3 +19,10 @@ def test_part1() -> None:
 
     aunts = day16.parse_aunt(TEST_DATA)
     assert "213" == day16.part1(aunts)
+
+
+def test_part2() -> None:
+    """Test Part 2"""
+
+    aunts = day16.parse_aunt(TEST_DATA)
+    assert "323" == day16.part2(aunts)
